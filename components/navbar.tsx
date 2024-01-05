06f09b14e9fe26bf1246acfdc7e26c1df36a863c
nomandhoni-cs <nomandhoni@yahoo.com>
2024-01-05 23:33:34 +0600

    Revambed Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,12 +8,8 @@ import {
   NavbarMenuItem,
 } from "@nextui-org/navbar";
 import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
 import { Link } from "@nextui-org/link";
-import { Input } from "@nextui-org/input";
-
 import { link as linkStyles } from "@nextui-org/theme";
-
 import { siteConfig } from "@/config/site";
 import NextLink from "next/link";
 import clsx from "clsx";
@@ -30,27 +26,6 @@ import {
 import { Logo } from "@/components/icons";
 
 export const Navbar = () => {
-  // const searchInput = (
-  // 	<Input
-  // 		aria-label="Search"
-  // 		classNames={{
-  // 			inputWrapper: "bg-default-100",
-  // 			input: "text-sm",
-  // 		}}
-  // 		endContent={
-  // 			<Kbd className="hidden lg:inline-block" keys={["command"]}>
-  // 				K
-  // 			</Kbd>
-  // 		}
-  // 		labelPlacement="outside"
-  // 		placeholder="Search..."
-  // 		startContent={
-  // 			<SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-  // 		}
-  // 		type="search"
-  // 	/>
-  // );
-
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -94,7 +69,6 @@ export const Navbar = () => {
           </Link>
           <ThemeSwitch />
         </NavbarItem>
-        {/* <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem> */}
         <NavbarItem className="hidden md:flex">
           <Button
             isExternal
@@ -118,7 +92,6 @@ export const Navbar = () => {
       </NavbarContent>
 
       <NavbarMenu>
-        {/* {searchInput} */}
         <div className="mx-4 mt-2 flex flex-col gap-2">
           {siteConfig.navMenuItems.map((item, index) => (
             <NavbarMenuItem key={`${item}-${index}`}>
