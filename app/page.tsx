a4f8a78ce43d1c8084b3e1bbeec56d26e68c8125
nomandhoni-cs <nomandhoni@yahoo.com>
2024-01-12 21:40:30 +0600

    Gave proxyt

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 // import { title, subtitle } from "@/components/primitives";
-import { GithubIcon, SearchIcon } from "@/components/icons";
 import { Kbd } from "@nextui-org/kbd";
 import { Input } from "@nextui-org/input";
 
